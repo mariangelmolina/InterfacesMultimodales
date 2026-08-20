6bb4b8a12b089a106b83ac7456341be433fba270
Mariangel Molina Botero <97903346+mariangelmolina@users.noreply.github.com>
2026-08-20 10:08:04 -0500

    Add header and image to Streamlit app

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("HOLA :) me llamo Mariangel")
 
+st.header("apps yay")
+st.write("aca voy a poner una foto")
+image = Image.open('DSC00365.JPG')
+st.image(image, caption='Interfaces multimodales')
+
